4e4f9e01f748739109bb4e7d86890a14f09c6151
Gerald9610 <gerald.mayu@gmail.com>
2020-06-01 22:31:45 -0600

    cambio de arreglo

--- Ejercicio3.php
+++ Ejercicio3.php
@@ -6,19 +6,31 @@
 echo "<h2>Arreglos</h2>";
 
 $miArreglo = array (10,20,30,40,50);
+
 echo "<p>Contenido del elemento 2 del arreglo: $miArreglo[2]</p>";
 echo "<p>Despliega el contenido del arreglo:</p>";
+
 print_r ($miArreglo);//se utiliza esta funcion para ver en forma entendible el contenido de la vvariable
+
 echo "<p>El tamaño del arreglo es :".sizeof($miArreglo)."</p>";
+
 $mifechaArr["dia"]="Lunes";
 $mifechaArr["mes"]="Mayo";
 $mifechaArr["año"]="2020";
 
 echo "<br><br>";
+
 var_dump ($mifechaArr);//muestra informacion de la variable indicada
+
 unset ($mifechaArr["año"]);//Elimina el elemento indicado del arreglo
+
 echo "<br><br>";
+print_r ($mifechaArr);
 $MiArregloReves = array_reverse($miArreglo);
+
+echo "<br><br>";
+print_r ($mifechaArr);
+
 ?>
 </body>
 </html>
